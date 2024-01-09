6fee3026473f9e72149c1342d467762b7a1752f3
rachidWassati <rachid.jeffali@wassati.com>
2024-01-09 18:31:38 +0100

    Gestion de l'erreur TokenExpiredError

--- src/middlewares/CommonAuth.ts
+++ src/middlewares/CommonAuth.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { AuthPayload } from "../dto/Auth.dto";
 import { isValidSignature } from "../utility";
+import { TokenExpiredError } from "jsonwebtoken";
 
 declare global {
     namespace Express {
@@ -11,10 +12,17 @@ declare global {
 }
 
 export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
-    const isValid = await isValidSignature(req);
-    if(isValid) {
-        next()
-    } else {
-        return res.status(401).json({message: "User not Authorized"})
+    try {
+        const isValid = await isValidSignature(req);
+        if(isValid) {
+            next()
+        } else {
+            return res.status(401).json({message: "User not Authorized"})
+        }
+    } catch (error) {
+        if (error instanceof TokenExpiredError) {
+            return res.status(401).json({message: "Token expired !"})
+        }
+        return res.status(401).json({message: "Authentication problem"})
     }
 }
