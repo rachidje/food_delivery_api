0453462267864e64a8f303fb37012797cc8989a5
rachidWassati <rachid.jeffali@wassati.com>
2024-01-13 18:54:19 +0100

    correction path to images

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@ import App from './services/ExpressApp';
 import dbConnection from './services/Database';
 
 const startServer = async () => {
+    
     const app = express()
     await dbConnection()
     await App(app)
-
+    
     app.listen(process.env.PORT || 8000, () =>{
         console.clear()
         console.log("✅ Server connection established")
         console.log("✅ Mongodb connection established")
+        console.log(process.env.NODE_ENV)
     })
 }
 
